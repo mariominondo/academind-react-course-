5d11d2ff8d01ffc03c47dc8a2d86512f1cf83e83
Minondo Pineda <Mario.Minondo@bantigua.com.gt>
2022-01-18 16:26:14 -0600

    Adding: test states in ExpensesItem.js

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,14 +1,24 @@
+import React, {useState} from 'react';
 import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
 
 const ExpenseItem = (props) => {
+  const [title, setTitle] = useState(props.expenses.title);
+  // console.log(1, title);
+
+  const titleHandler = () => {
+    setTitle('Hola!');
+    // console.log(title);
+  };
+
   return (
     <div className='expense-item'>
       <ExpenseDate date={props.expenses.date} />
       <div className='expense-item__description'>
-        <h2>{props.expenses.title}</h2>
+        <h2>{title}</h2>
         <div className='expense-item__price'>${props.expenses.amount}</div>
       </div>
+      <button onClick={titleHandler}>Change title</button>
     </div>
   );
 }
